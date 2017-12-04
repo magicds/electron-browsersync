bafcf3bd91b24347debec00d2bbd15b5af87c14d
cdswyda <cdswyda@hotmail.com>
2017-12-04 11:03:16 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-console": "warn",
+    "no-unused-vars": "warn"
+  }
+}
